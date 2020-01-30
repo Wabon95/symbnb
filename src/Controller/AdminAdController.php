a5858be7cb5b41784f978f0c23154bfdd0f94580
Wabon95 <dbonaglia95@protonmail.com>
2020-01-30 19:54:02 +0100

    admincontroller

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,6 +16,21 @@ class AdminAdController extends AbstractController {
     public function index(AdRepository $ar) {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $ar->findAll()
+        ]);
+    }
+
+    /** @Route("/admin/ads/{id}/edit", name="admin_ads_edit") */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+            $this->addFlash('success', "L'annonce <strong>{$ad->getTitle()}</strong> à bien été enregistrée !");
+        }
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
